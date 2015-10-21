3c28b86f29dad2b47443f55434c44d666d247708
Sai Pc <schand31@asu.edu>
2015-10-21 03:14:48 -0700

    numbers

---
+++ numbers.py
@@ -0,0 +1,10 @@
+__author__ = 'schandramouli'
+
+
+def compare(x):
+    if(10 < x <12):
+        print "hell yeah"
+        x = True
+        if x == True:
+            print "yeah"
+        print "well this sucks"
